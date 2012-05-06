b62b5f3498f3c7a91b31b9dd11b2f2c44c206ef7
bsaleil <baptiste.saleil@gmail.com>
2012-05-06 18:11:26 +0200

    gnu header

--- rarserver.py
+++ rarserver.py
@@ -1,3 +1,21 @@
+# This script is inspired by : http://www.navarin.de/projects/rbox/
+# Website ans repository : https://github.com/bsaleil/rhythmbox-android-remote
+
+#Copyright (C) 2009 Baptiste Saleil
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #!/usr/bin/python
 
 # TODO :
